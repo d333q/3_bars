200e246ecc1e50477a79feda7f0c8a50554d94a6
d333q <d33q@yandex.ru>
2016-09-22 23:04:27 +0000

    Надо разобраться со списками, выводит максимальный количество гостей в баре(без названия бара)

--- bars.py
+++ bars.py
@@ -1,14 +1,30 @@
 import json
-
+from pprint import pprint
 
 def load_data(filepath):
-    pass
-
+    with open(filepath) as json_file:
+        json_data = json.loads(json_file.read())
+        return json_data
+        #print(json_data[2]["Cells"]["Name"])
+        #data = json_data[2]["Cells"]["Name"]
+        #print (data)        
 
 def get_biggest_bar(data):
-    pass
-
-
+    name=[]
+    guests=[]
+    i=0
+    while i<515:
+        name.append(data[i]["Cells"]["Name"])
+        guests.append(data[i]["Cells"]["SeatsCount"])
+        i=i+1
+        #pprint (name)
+    print(guests.index(max(guests)))
+        
+    #while i<514:
+     #   for key, value in data[i]["Cells"]["geoData"].items():
+      #      print ('%s: %s' % (key, value))
+       #     i=i+1
+    
 def get_smallest_bar(data):
     pass
 
@@ -18,4 +34,5 @@ def get_closest_bar(data, longitude, latitude):
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data('barsjson.json')
+    get_biggest_bar(data)
