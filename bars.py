745d2a6f7b9f3bff286167f232092ef42ff033aa
d333q <d33q@yandex.ru>
2016-09-24 15:29:20 +0000

    ok

--- bars.py
+++ bars.py
@@ -1,38 +1,77 @@
 import json
-from pprint import pprint
+import math
+
 
 def load_data(filepath):
-    with open(filepath) as json_file:
-        json_data = json.loads(json_file.read())
-        return json_data
-        #print(json_data[2]["Cells"]["Name"])
-        #data = json_data[2]["Cells"]["Name"]
-        #print (data)        
+    data_json = open(filepath)
+    data = json.load(data_json)
+    return data
+
 
 def get_biggest_bar(data):
-    name=[]
-    guests=[]
-    i=0
-    while i<515:
-        name.append(data[i]["Cells"]["Name"])
-        guests.append(data[i]["Cells"]["SeatsCount"])
-        i=i+1
-        #pprint (name)
-    print(guests.index(max(guests)))
-        
-    #while i<514:
-     #   for key, value in data[i]["Cells"]["geoData"].items():
-      #      print ('%s: %s' % (key, value))
-       #     i=i+1
-    
+    i = 0
+    y = 0
+    bar_guests = []
+    while i < len(data):
+        bar = data[i]['Cells']['SeatsCount']
+        bar_guests.append(bar)
+        i = i+1
+    max = bar_guests[y]
+    while y < len(bar_guests):
+        if bar_guests[y] > max:
+            max = bar_guests[y]
+        y = y + 1
+    index = bar_guests.index(max)
+    name = data[index]['Cells']['Name']
+    print ("Самый большой бар: "+name)
+    print (max)
+
+
 def get_smallest_bar(data):
-    pass
+    i = 0
+    bar_guests = []
+    while i < len(data):
+        bar = data[i]['Cells']['SeatsCount']
+        bar_guests.append(bar)
+        i = i+1
+    y = 0
+    min = bar_guests[y]
+    while y < len(bar_guests):
+        if bar_guests[y] < min:
+            min = bar_guests[y]
+        y = y + 1
+    index = bar_guests.index(min)
+    name = data[index]['Cells']['Name']
+    print ("Самый маленький бар: "+name)
+    print (min)
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
-
-
+    longitude = math.radians(longitude)
+    latitude = math.radians(latitude)
+    allcoordinates = []
+    R = 6371
+    i = 0
+    while i < len(data):
+        bars_longitude = math.radians(
+            data[i]['Cells']['geoData']['coordinates'][0])
+        bars_latitude = math.radians(
+            data[i]['Cells']['geoData']['coordinates'][1])
+        d = math.acos(
+            (math.sin(bars_latitude) * math.sin(latitude)) +
+            (math.cos(bars_latitude) *
+             math.cos(latitude) * math.cos(bars_longitude-longitude)))
+        distance = d*R
+        allcoordinates.append(distance)
+        i = i+1
+    name = min(allcoordinates)
+    name_bars = data[allcoordinates.index(name)]['Cells']['Name']
+    print("Ближайшее заведение : " + name_bars)
+    print(min(allcoordinates))
 if __name__ == '__main__':
     data = load_data('barsjson.json')
     get_biggest_bar(data)
+    get_smallest_bar(data)
+    a = float(input('Введите долготу '))
+    b = float(input('Введите широту '))
+    get_closest_bar(data, a, b)
