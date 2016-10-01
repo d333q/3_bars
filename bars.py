96d1e3a863749189dcbeb9b0715513a9da4eada5
d333q <d33q@yandex.ru>
2016-10-01 10:24:13 +0000

    corrected

--- bars.py
+++ bars.py
@@ -6,42 +6,26 @@ import os
 def load_data(filepath):
     if not os.path.exists(filepath):
         return None
-    with open(filepath, 'r') as data_json:
-        return json.load(data_json)
+    with open(filepath, 'r') as json_content:
+        return json.load(json_content)
 
 
 def get_biggest_bar(data):
-    i = 0
-    y = 0
-    bar_guests = []
-    while i < len(data):
-        bar_guests.append(data[i]['Cells']['SeatsCount'])
-        i = i+1
-    max = bar_guests[y]
-    while y < len(bar_guests):
-        if bar_guests[y] > max:
-            max = bar_guests[y]
-        y = y + 1
-    index_bar = bar_guests.index(max)
-    name_bar = data[index_bar]['Cells']['Name']
-    return name_bar, max
+    allseatscount = []
+    for seatscount in data:
+        allseatscount.append(seatscount['Cells']['SeatsCount'])
+    namebar = data[allseatscount.index(max(allseatscount))]['Cells']['Name']
+    seatscount = max(allseatscount)
+    return namebar, seatscount
 
 
 def get_smallest_bar(data):
-    i = 0
-    bar_guests = []
-    while i < len(data):
-        bar_guests.append(data[i]['Cells']['SeatsCount'])
-        i = i+1
-    y = 0
-    min = bar_guests[y]
-    while y < len(bar_guests):
-        if bar_guests[y] < min:
-            min = bar_guests[y]
-        y = y + 1
-    index_bar = bar_guests.index(min)
-    name_bar = data[index_bar]['Cells']['Name']
-    return name_bar, min
+    allseatscount = []
+    for seatscount in data:
+        allseatscount.append(seatscount['Cells']['SeatsCount'])
+    namebar = data[allseatscount.index(min(allseatscount))]['Cells']['Name']
+    seatscount = min(allseatscount)
+    return namebar, seatscount
 
 
 def get_closest_bar(data, longitude, latitude):
@@ -49,22 +33,20 @@ def get_closest_bar(data, longitude, latitude):
     latitude = math.radians(latitude)
     allcoordinates = []
     earth_radius = 6371
-    i = 0
-    while i < len(data):
+    for coordinates_data in data:
         bars_longitude = math.radians(
-            data[i]['Cells']['geoData']['coordinates'][0])
+            coordinates_data['Cells']['geoData']['coordinates'][0])
         bars_latitude = math.radians(
-            data[i]['Cells']['geoData']['coordinates'][1])
+            coordinates_data['Cells']['geoData']['coordinates'][1])
         calculation = math.acos(
             (math.sin(bars_latitude) * math.sin(latitude)) +
             (math.cos(bars_latitude) *
              math.cos(latitude) * math.cos(bars_longitude-longitude)))
         distance = calculation*earth_radius
         allcoordinates.append(distance)
-        i = i+1
-    index_bar = min(allcoordinates)
-    name_bar = data[allcoordinates.index(index_bar)]['Cells']['Name']
-    return name_bar, min(allcoordinates)
+    namebar = data[allcoordinates.index(min(allcoordinates))]['Cells']['Name']
+    seatscount = min(allcoordinates)
+    return namebar, seatscount
 
 
 if __name__ == '__main__':
